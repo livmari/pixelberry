fce3d312729b320013e876e4840715a7d1b4c4f8
liivmarii <liv.alterskjaer@icloud.com>
2020-05-23 16:41:21 +0200

    Main content included in App.js and extra styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './css/App.css';
-import Hero from "./components/hero/Hero";
+import Hero from './components/hero/Hero';
+import Section from "./components/main/Section";
 
 function App() {
-  return (
-    <div className="app">
-      <Hero />
-    </div>
-  );
+    return (
+        <div className='app container'>
+            <Hero/>
+            <Section/>
+        </div>
+    );
 }
 
 export default App;
