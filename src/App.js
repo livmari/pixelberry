b86411603d9a089bf98a6a6e16b928df5ae5e4c8
liivmarii <liv.alterskjaer@icloud.com>
2020-05-25 23:18:23 +0200

    Redundant 'Main' component removed, all it did was play host

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import './css/App.css';
 import Header from './components/contents/Header';
-import Main from './components/contents/Main';
 import Footer from './components/contents/Footer';
 import Contact from './components/slides/Contact';
 import Faq from './components/slides/Faq';
 import Portfolio from './components/slides/Portfolio';
 import Projects from './components/slides/Projects';
+import Welcome from "./components/contents/Welcome";
+import Work from "./components/contents/Work";
 
 function App() {
     return (
         <div className='app'>
             <div className={'background-layer'}>
                 <Header/>
-                <Main/>
+                <Welcome/>
+                <Work/>
                 <Footer/>
             </div>
             <Portfolio/>
