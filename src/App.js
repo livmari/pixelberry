69f767867a187fe9b5a94179f2034b24e46f9936
liivmarii <liv.alterskjaer@icloud.com>
2020-05-25 12:31:28 +0200

    Update to reflect previous changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './css/App.css';
-import Hero from './components/hero/Hero';
-import Welcome from "./components/main/Welcome";
-import Services from "./components/main/Services";
+import Header from './components/header/Header';
+import Main from './components/main/Main';
+import Footer from './components/footer/Footer';
 
 function App() {
     return (
-        <div className='app container'>
-            <Hero/>
-            <Welcome/>
-            <Services/>
+        <div className='app'>
+            <Header/>
+            <Main/>
+            <Footer/>
         </div>
     );
 }
