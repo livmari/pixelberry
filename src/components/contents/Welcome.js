c2015918c34993aaf5b36f80b536360c6c357501
liivmarii <liv.alterskjaer@icloud.com>
2020-05-26 15:59:37 +0200

    Text content added

--- src/components/contents/Welcome.js
+++ src/components/contents/Welcome.js
@@ -21,12 +21,15 @@ class Welcome extends React.Component {
                 <div className={'row'}>
                     <div className={'twelve columns'}>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid cum delectus dolores
-                            eos
-                            est eum, impedit ipsa minima modi, molestias odio odit recusandae tempora unde ut vel?
-                            Architecto autem consequuntur, dolore, doloribus ea eligendi eos eveniet ipsa modi natus
-                            numquam
-                            odio perferendis quas quis quo suscipit veniam voluptas voluptatum!
+                            My name is Liv, and I'm a freelance developer based in Gothenburg, Sweden. I love meeting
+                            new people, and creating value in their daily lives by developing web based solutions that
+                            help solve problems they're facing.
+                        </p>
+                        <p>
+                            My ultimate goal is to make my customers and partners feel like the very zen dude at the
+                            center of this website design. By working with me, I hope that you feel like complex tasks
+                            are solved with simplified but solid solutions, that are easy for you to take ownership of
+                            and build from.
                         </p>
                     </div>
                 </div>
